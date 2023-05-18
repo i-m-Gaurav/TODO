e1cae51836ba1045c0deb574f88b7d997c573fc4
unknown <indiagauravkumar@gmail.com>
2023-05-18 13:29:57 +0530

    added a seperate list work you can go through the /work

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -9,8 +10,10 @@ app.use(express.static("public"));
 app.set("view engine","ejs");
 
 let items = [];
+let workItems= [];
 
 app.get("/", function(req,res){
+
     var today = new Date();
     var currentDay = today.toLocaleDateString('en-US', { weekday: 'long' , day: 'numeric', month: 'long'});
     var day = "";
@@ -22,16 +25,36 @@ app.get("/", function(req,res){
         day = currentDay;
     }
 
-    res.render("list",{day: day,newItem : items});
+    res.render("list",{ListTitle: day,newItem : items});
 });
 
 app.post("/", function(req,res){
+
+    console.log(req.body)
     let item = req.body.add;
 
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list === "Work"){
+workItems.push(item);
+res.redirect("/work")
+    }
+    else
+    {
+        items.push(item);
+        res.redirect("/");
+
+
+    }
+
     
 })
+
+app.get("/work", function(req,res){
+
+    res.render("list",{ListTitle: "Work List", newItem : workItems});
+
+})
+
+
 
 app.listen(3000 , function(){
     console.log("Server is running on 3000");
